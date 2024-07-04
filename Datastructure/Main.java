86229bf41f90530d98152d41d04386ca448e5070
HR-04 <hariniregarajan203@gmail.com>
2024-07-04 10:12:15 +0530

    Add : Constructor

---
+++ Datastructure/Main.java
@@ -0,0 +1,18 @@
+class Dog{
+    private String name;
+    private int age;
+    Dog(){
+        System.out.println("hehehe");
+    }
+
+    Dog(int x){
+        this.age;
+        System.out.println(age);
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        Dog dg = new Dog(x:0); //Creation of object for dog class.
+    }
+}
