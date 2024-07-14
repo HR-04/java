eba3795309900a684f0fe33ef4a59a71aa69be54
HR-04 <harinirengarajan203@gmail.com>
2024-07-14 19:23:41 +0530

    Add : Multiplication of no

---
+++ Training1/multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class multiplication {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Numbers:");
+       int n = sc.nextInt();
+       for(int i=1;i<=10;i++){
+           System.out.println(i+" * " + n+ " = "+ i*n );
+       }
+    }
+}
