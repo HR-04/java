29c909c4488315f0ee5c54380d6902214c640f69
HR-04 <harinirengarajan203@gmail.com>
2024-07-14 19:39:25 +0530

    Add : Print Even Numbers

---
+++ Training1/Evenno.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Evenno {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        for(int i = 2;i<=20;i+=2){
+            System.out.println(i);
+        }
+    }
+}
